822714e34bf77b4df84dc36e0ee9360414e23ff6
user <user@DESKTOP-1VTN10N>
2023-08-19 17:41:35 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problema_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clienti_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clienti_Click_Mod(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clienti_Click_Del(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clienti_Click_Viz(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Activitati_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Activitati_Click_Mod(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Activitati_Click_Del(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Activitati_Click_Viz(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rezervari_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rezervari_Click_Mod(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rezervari_Click_Del(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rezervari_Click_Viz(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
